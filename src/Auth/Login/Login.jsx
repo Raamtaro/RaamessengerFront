5f72ddb84ca8e354c3c6c998370db1f981036737
Raam <raam.sanghani@gmail.com>
2024-10-29 21:38:24 -0500

    Set up the Login/Signup forms, next is to set up the Logged-in routes

--- src/Auth/Login/Login.jsx
+++ src/Auth/Login/Login.jsx
@@ -1,11 +1,77 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import './styles/Login.css'
 
 function Login() {
+
+  const navigate = useNavigate()
+  const [loading, setLoading] = useState(false)
+
+  const [error, setError] = useState(false)
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    const formData = new FormData(event.target)
+    const data = Object.fromEntries(formData.entries());
+
+    /**
+     * Make the API call to our endpoint, which, for now, will be at locahost:3000
+     * MessengerServer is hosted on railway, and so the POST target will be updated as such when hosted
+     */
+    setLoading(true)
+    try {
+      const response = await fetch(
+        'http://localhost:3000/auth/login',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        }
+      )
+
+      if (response.ok) {
+        const result = await response.json()
+        // console.log(result)
+
+        localStorage.setItem('user', JSON.stringify(result.user))
+        localStorage.setItem('token', result.token)
+
+      }
+    } catch (err) {
+      setError(true)
+      console.error(err.message)
+    } finally {
+      setLoading(false)
+    }
+
+  }
+
   return (
     <>
-        <section className='login-section'>
+      <section className='login-section'>
+          <form onSubmit={handleSubmit} className="login-form">
+
+            <div className="form-inputs">
+              <div className="form-section">
+                <input type="email" id="email" name="email" placeholder="info@mailaddress.com" autoComplete='on' required/>
+                <label htmlFor="email">Email</label>
+              </div>
+
+              <div className="form-section">
+                <input type="password" id="password" name="password" placeholder="••••••••••••" autoComplete='on' required/>
+                <label htmlFor="password">Password</label>
+              </div>
+            </div>
             
-        </section>
+            <button type="submit">Login</button>
+
+          </form>
+      </section>
     </>
   )
 }
--- src/Auth/Signup/Signup.jsx
+++ src/Auth/Signup/Signup.jsx
@@ -1,10 +1,75 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import './styles/Signup.css'
 
 function Signup() {
+  const navigate = useNavigate()
+
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    /**
+     * Post to the signup route 
+     */
+
+    const formData = new FormData(event.target)
+    const data = Object.fromEntries(formData.entries());
+    setLoading(true)
+    try {
+      const response = await fetch(
+        'http://localhost:3000/auth/signup',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        }
+      )
+      if (response.ok) {
+        const result = await response.json()
+        console.log(result)
+      }
+    } 
+
+    catch (err) {
+      setError(true)
+      console.error(err.message)
+    } 
+
+    finally {
+      setLoading(false)
+    }
+
+  }
+
   return (
     <>
         <section className="signup-section">
-            
+            <form onSubmit={handleSubmit} className='signup-form'>
+              <div className="form-inputs">
+                <div className="form-section">
+                  <input type="text" id="name" name="name" placeholder="Joe Bob Sue Due" autoComplete='on' required/>
+                  <label htmlFor="email">Name</label>
+                </div>
+                <div className="form-section">
+                  <input type="email" id="email" name="email" placeholder="info@mailaddress.com" autoComplete='on' required/>
+                  <label htmlFor="email">Email</label>
+                </div>
+
+                <div className="form-section">
+                  <input type="password" id="password" name="password" placeholder="••••••••••••" autoComplete='on' required/>
+                  <label htmlFor="password">Password</label>
+                </div>
+              </div>
+              
+              <button type="submit">Signup</button>
+            </form>
         </section>
     </>
   )
