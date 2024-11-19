257ce51f4ecfd3b097610513922482fd5a5f8d38
Raam <raam.sanghani@gmail.com>
2024-11-18 18:49:11 -0600

    Basic message display has been implemented in the conversation control - I'm currently just working off of one state within the ConversationControl.jsx component. Depending on how this develops in the future, it could be worth creating a state specifically for messages. " '

--- src/Logged-In-Views/UserMain/NestedRoutes/ConversationControl/ConversationControl.jsx
+++ src/Logged-In-Views/UserMain/NestedRoutes/ConversationControl/ConversationControl.jsx
@@ -1,19 +1,21 @@
 import React, {useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom'
 
-
-
-
-
 function ConversationControl() {
     const {id} = useParams()
 
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState()
+
     const [conversation, setConversation] = useState()
+    
 
     useEffect(()=> {
-        const token = localStorage.getItem('token')
 
+        const token = localStorage.getItem('token')
+        
         const retrieveConversation = async() => {
+            setLoading(true)
             try {
                 const response = await fetch(
                     `${import.meta.env.VITE_SERVER_URL}/conversation/${id}`,
@@ -31,22 +33,45 @@ function ConversationControl() {
 
                 const result = await response.json()
                 console.log(result) //Successful
+                setConversation(result.conversation)
 
             } catch(err) {
                 console.error('Error:', err)
+                setError(err)
+            } finally {
+                setLoading(false)
             }
         }
 
         retrieveConversation()
 
-    },[])
+    },[id])
+
+    // useEffect(()=> {
+    //     console.log(conversation.id)
+    // }, [conversation])
     
 
     
     return (
         <>
-        
-            
+            <div className="conversation-viewer">
+                <h3>{conversation?.id}</h3> 
+                <div className="sent-messages">
+                    {
+                        conversation.messages.map(
+                            (message)=>(
+                                <span
+                                    key={message.id}
+                                    className={'conversation-message'}
+                                >
+                                    {message.body}
+                                </span>
+                            )
+                        )
+                    }
+                </div>
+            </div>           
         </>
     )
 }
